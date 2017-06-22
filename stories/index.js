bf8ed4506e51bc96fd3ed147c9ebd93267eec4e4
metasean <metasean@users.noreply.github.com>
2017-06-21 22:41:09 -0600

    storybook added, but not working :-(

---
+++ stories/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import Header from '../components/Header'
+
+storiesOf('Header', module)
+  .add('with navigation div', () => <Header />)
+
+/*
+import Button from '@storybook/components/dist/demo/Button';
+import Welcome from '@storybook/components/dist/demo/Welcome';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+*/
